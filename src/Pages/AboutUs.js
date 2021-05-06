7ddf0ba1fa0e709b0bde338ee2bce3b6e9d74c9e
andesob <andesob@stud.ntnu.no>
2021-05-06 21:58:57 +0200

    Added homepage

---
+++ src/Pages/AboutUs.js
@@ -0,0 +1,13 @@
+import {Component} from "react/cjs/react.production.min";
+
+class AboutUs extends Component {
+    render() {
+        return (
+            <div>
+                About us
+            </div>
+        )
+    }
+}
+
+export default AboutUs;
